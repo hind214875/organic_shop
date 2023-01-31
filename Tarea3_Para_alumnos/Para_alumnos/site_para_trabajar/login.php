677acd42f3e3d2d5e4010c5263eba6333348ccaf
hind <hindsamiri2@gmail.com>
2023-01-31 12:47:59 +0100

    add comments

--- Tarea3_Para_alumnos/Para_alumnos/site_para_trabajar/login.php
+++ Tarea3_Para_alumnos/Para_alumnos/site_para_trabajar/login.php
@@ -1,30 +1,45 @@
 <?php
+
+require_once "config.php";
 session_start();
-include "config.php";
+
 
 if (isset($_SESSION['username'])) {
     header("location:home.tpl.php");
     die();
 }
+
 //connect to database
 $db = connectDB();
 if ($db) {
-    if (isset($_POST['login_btn'])) {
-        $username = mysqli_real_escape_string($db, $_POST['username']);
-        $password = mysqli_real_escape_string($db, $_POST['password']);
-        $password = md5($password); //hashed password before storing last time
+    //EN CASO DE PULSAR EL BOTÓN DE SUBMIT EN EL LOGIN
+    if (filter_input(INPUT_POST, 'login_btn', FILTER_SANITIZE_SPECIAL_CHARS) !== null) {
 
-        $sql = "SELECT * FROM users WHERE  user_name='$username' AND password='$password'";
-        $result = mysqli_query($db, $sql);
+        //SI VIENEN LOS DATOS Y NO ESTÁN VACÍOS
+        if (isset($_POST['username']) && ($_POST['username'] !== "") && (isset($_POST['password']) && ($_POST['password'] !== ""))) {
+            //TOMAMOS USUARIO Y CONTRASEÑA DEL POST
+            $username = mysqli_real_escape_string($db, $_POST['username']);
+            $password = mysqli_real_escape_string($db, $_POST['password']);
+            $password = md5($password); //hashed password before storing last time
 
-        if ($result) {
-
-            if (mysqli_num_rows($result) >= 1) {
-                $_SESSION['message'] = "You are now Loggged In";
-                $_SESSION['username'] = $username;
-                header("location:home.tpl.php");
-            } else {
-                $_SESSION['message'] = "Username and Password combiation incorrect";
+            //SENTENCIA SQL PARA VER QUE TANTO EL USUARIO COMO LA CONTRASEÑA COINCIDEN CON ALGUNA en BD
+            $sql = "SELECT * FROM users WHERE  user_name='$username' AND password='$password'";
+            //EJECUTAMOS LA SENTENCIA
+            $result = mysqli_query($db, $sql);
+            
+            if ($result) {
+                //SI HAY ALGÚN RESULTADO
+                if (mysqli_num_rows($result) >= 1) {
+            //GUARDAMOS EL NOMBRE DE USUARIO EN LA VARIABLE DE SESION username y message en variable de session message
+                    $_SESSION['message'] = "You are now Loggged In";
+                    $_SESSION['username'] = $username;
+                    echo $_SESSION['message'];
+                     //VOLVEMOS AL INDEX
+                    header("location:home.tpl.php");
+                } else {
+                    $_SESSION['message'] = "Username and Password combination incorrect, please try again";
+                    echo $_SESSION['message'];
+                }
             }
         }
     }
@@ -39,25 +54,26 @@ if ($db) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./css/loginregist.css">
-    
+
 </head>
 
 <body>
     <div class="d-flex justify-content-center align-items-center vh-100">
-       <?php
-       /*
+        <?php
+        /*
         if (isset($_SESSION['message'])) {
-            echo "<div id='error_msg'>" . $_SESSION['message'] . "</div>";
-            unset($_SESSION['message']);
+        echo "<div id='error_msg'>" . $_SESSION['message'] . "</div>";
+        unset($_SESSION['message']);
         }*/
         ?>
-       
+
         <form class="shadow w-450 p-3" action="login.php" method="post">
 
             <h4 class="display-4  fs-1">LOGIN</h4><br>
-            
+
             <div class="mb-3">
                 <label class="form-label">User name</label>
                 <input type="text" class="form-control" name="username">
@@ -71,8 +87,8 @@ if ($db) {
             <button type="submit" class="btn btn-primary" name="login_btn">Login</button>
             <a href="register.php" class="link-secondary">Sign Up</a>
         </form>
-        </div> 
-    
+    </div>
+
 </body>
 
 </html>
--- Tarea3_Para_alumnos/Para_alumnos/site_para_trabajar/register.php
+++ Tarea3_Para_alumnos/Para_alumnos/site_para_trabajar/register.php
@@ -1,40 +1,53 @@
 <?php
 session_start();
 include "config.php";
+
 //connect to database
 $db = connectDB();
+//si la conexión a la base de datos tiene éxito
 if ($db) {
+
+
   //si el btn register clicked
   if (isset($_POST['register_btn'])) {
-    //cogimos datos de entrada
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $password = mysqli_real_escape_string($db, $_POST['password']);
-    $password2 = mysqli_real_escape_string($db, $_POST['password2']);
+    //SI VIENEN LOS CAMPOS username,password y confirmacion password Y NO ESTÁN VACÍOS
+    if ((isset($_POST['username']) && ($_POST['username'] !== "")) && (isset($_POST['password']) && ($_POST['password'] !== "")) && (isset($_POST['password2']) && ($_POST['password2'] !== ""))) {
+      //cogimos datos de de usuario y contraseña
+      $username = mysqli_real_escape_string($db, $_POST['username']);
+      $password = mysqli_real_escape_string($db, $_POST['password']);
+      $password2 = mysqli_real_escape_string($db, $_POST['password2']);
 
-    //verficacion si el user ya existe
-    $query = "SELECT * FROM users WHERE user_name = '$username'";
-    $result = mysqli_query($db, $query);
+      //verficacion si el user ya existe
+      $query = "SELECT * FROM users WHERE user_name = '$username'";
+       //EJECUTAMOS LA SENTENCIA
+      $result = mysqli_query($db, $query);
 
-    if ($result) {
-      //si el resultado duelve algo si este usuario existe
-      if (mysqli_num_rows($result) > 0) {
+      if ($result) {
+        //si el resultado duelve algo si este usuario existe
+        if (mysqli_num_rows($result) > 0) {
 
-        echo '<script language="javascript">';
-        echo 'alert("Username already exists")';
-        echo '</script>';
-      } else {
-        // si no creamos el nuevo usuario
-        if ($password == $password2) {           //Create User
-          $password = md5($password); //hash password before storing for security purposes
-          $sql = "INSERT INTO users(user_name, password ) VALUES('$username','$password')";
-          mysqli_query($db, $sql);
-
-          $_SESSION['message'] = "You are now logged in";
-          $_SESSION['username'] = $username;
-
-          header("location:home.tpl.php");  //redirect home page
+          echo '<script language="javascript">';
+          echo 'alert("Username already exists")';
+          echo '</script>';
         } else {
-          $_SESSION['message'] = "The two password do not match";
+          // si no creamos el nuevo usuario
+          if ($password == $password2) { //si la contraseña match con la confirmacion
+            $password = md5($password); //hash contraseña antes de almacenar por motivos de seguridad
+            //CREAMOS UNA SENTENCIA QUE INSERTE LOS DATOS
+            $sql = "INSERT INTO users(user_name, password ) VALUES('$username','$password')";
+            //EJECUTAMOS LA SENTENCIA $sql
+            mysqli_query($db, $sql);
+             //guardamos el usuario y message en variable session
+            $_SESSION['message'] = "You are now logged in";
+            $_SESSION['username'] = $username;
+            echo  $_SESSION['message'] ;
+            header("location:home.tpl.php"); //redirect home page
+          } else {
+            $_SESSION['message'] = "The two password do not match";
+            echo '<script language="javascript">';
+            echo 'alert("'.$_SESSION['message'].'")';
+            echo '</script>';
+          }
         }
       }
     }
